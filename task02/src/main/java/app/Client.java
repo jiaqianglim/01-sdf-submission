de04c5fd94eed4d6850638c19e0b9d86d05a7f92
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-07-01 12:23:19 +0800

    Task2 final commit

--- task02/src/main/java/app/Client.java
+++ task02/src/main/java/app/Client.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -21,45 +22,51 @@ public class Client {
     
     public void start() throws IOException{
         Socket s = new Socket("task02.chuklee.com", 80);
-
-        try(OutputStream os = s.getOutputStream();
+        while(true){
+            try(OutputStream os = s.getOutputStream();
             InputStream is = s.getInputStream()){
                 ObjectOutputStream oos = new ObjectOutputStream(os);
                 ObjectInputStream ois = new ObjectInputStream(is);
-
                 String irequest = ois.readUTF();
                 String irequestid = irequest.split(" ")[0];
-                String[] irequestnumlist = irequest.split(" ")[1].split(",");
+                String irequestnumliststring = irequest.split(" ")[1];
+                String[] irequestnumlist = irequestnumliststring.split(",");
                 for (String i: irequestnumlist){
                     try{
                         sum += Float.parseFloat(i);
                     }catch (NumberFormatException e){
                         System.out.println("the number in the list is not a valid number"  + e);
                     }
+                        noint++;
                 }
                 float average = sum/noint;
-
-                oos.writeUTF(irequest);
+                oos.writeUTF(irequestid);
                 oos.writeUTF("Lim Jia Qiang");
                 oos.writeUTF("jqlimjiaqiang@gmail.com");
                 oos.writeFloat(average);
                 oos.flush();
 
                 sresponse = ois.readBoolean();
-                System.out.println(sresponse);
+                if(sresponse){
+                    System.out.println("Success");
+                }else{
+                    String fsresponse = ois.readUTF();
+                    System.out.println("Failed" + fsresponse);
+                }
                 
                 oos.close();
                 os.close();
                 ois.close();
                 is.close();
-                
+                break;
 
-            }catch(IOException e){
+            }catch(EOFException e){
+                System.out.println(e);
+            } catch(IOException e){
                 System.out.println(e);
             }finally{
                 System.out.println("Task 2 finished");
             }
-
+        }
     }
-    
 }
