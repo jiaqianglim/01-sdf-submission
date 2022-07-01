977a937c9b84d3ac530dd5c8ad91935c5c629e3b
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-07-01 10:04:09 +0800

    task 02 90%

--- task02/src/main/java/app/App.java
+++ task02/src/main/java/app/App.java
@@ -1,13 +1,16 @@
 package app;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        Client c = new Client();
+        c.start();
     }
 }
--- task02/src/main/java/app/Client.java
+++ task02/src/main/java/app/Client.java
@@ -8,12 +8,19 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class Client {
+
+    private Float sum = 0.0f;
+    private Float noint = 0.0f;
+    private Float average; 
+    private String irequest;
+    private boolean sresponse;
+
     public Client(){
 
     }
     
     public void start() throws IOException{
-        Socket s = new Socket("sdf.chuklee.com", 80);
+        Socket s = new Socket("task02.chuklee.com", 80);
 
         try(OutputStream os = s.getOutputStream();
             InputStream is = s.getInputStream()){
@@ -23,11 +30,34 @@ public class Client {
                 String irequest = ois.readUTF();
                 String irequestid = irequest.split(" ")[0];
                 String[] irequestnumlist = irequest.split(" ")[1].split(",");
+                for (String i: irequestnumlist){
+                    try{
+                        sum += Float.parseFloat(i);
+                    }catch (NumberFormatException e){
+                        System.out.println("the number in the list is not a valid number"  + e);
+                    }
+                }
+                float average = sum/noint;
 
+                oos.writeUTF(irequest);
+                oos.writeUTF("Lim Jia Qiang");
+                oos.writeUTF("jqlimjiaqiang@gmail.com");
+                oos.writeFloat(average);
+                oos.flush();
+
+                sresponse = ois.readBoolean();
+                System.out.println(sresponse);
+                
+                oos.close();
+                os.close();
+                ois.close();
+                is.close();
                 
 
             }catch(IOException e){
                 System.out.println(e);
+            }finally{
+                System.out.println("Task 2 finished");
             }
 
     }
